84d3e71d7eb7a95f5354135f4d34bc7f934ba5d0
Ana Laura Chenoweth Galaz <analaura@Anas-MacBook-Air.local>
2025-07-30 16:00:12 -0400

    Creating a circular list for the problem of josephus

--- main.cpp
+++ main.cpp
@@ -1,8 +1,21 @@
 #include <iostream>
+#include "ListaCircular/ListaCircular.hpp"
 
 using namespace std;
 
 int main() {
+    // Crear un c�rculo de personas con valores del 1 al 5
+    ListaCircular<string> josephusCirculo;
+    cout << "**********************************************************\n"
+    << "               CIRCULO DE JOSEPH                                        \n"
+    << "                                                          \n";
+    cout << "Numero de personas: ";
 
+    int n;
+    do{
+        cin >> n;
+        if(n>=100) cout <<"Introduce un numero menor a " << 100 << ": ";
+        if(n<1) cout <<"Introduce un numero mayor a " << 1 << ": ";
+    }while(n>=100 ||n<1);
     return 0;
 }
