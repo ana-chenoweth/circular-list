1b985388330ffbce402b20fc5386ae8318538ef9
Ana Laura Chenoweth Galaz <analaura@Anas-MacBook-Air.local>
2025-07-30 13:04:59 -0400

    Creating the class ListaCircular for a circular list

---
+++ ListaCircular/ListaCircular.hpp
@@ -0,0 +1,24 @@
+#ifndef LISTACIRCULAR_HPP_INCLUDED
+#define LISTACIRCULAR_HPP_INCLUDED
+
+template <typename T>
+class ListaCircular {
+private:
+    struct Elemento {
+        T valor;
+        Elemento* siguiente;
+        Elemento* anterior;
+        Elemento(T v, Elemento* sig = nullptr, Elemento* ant = nullptr)
+            : valor(v), siguiente(sig), anterior(ant) {}
+    };
+    int tam;
+    Elemento* cabeza;
+
+public:
+    ListaCircular();
+
+};
+
+#include "ListaCircular.tpp"
+
+#endif // LISTACIRCULAR_HPP_INCLUDED
---
+++ ListaCircular/ListaCircular.tpp
@@ -0,0 +1,3 @@
+template <typename T>
+ListaCircular<T>::ListaCircular() : tam(0), cabeza(nullptr) {}
+//***********************************************************************
